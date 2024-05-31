705390ee6622aa88e55b9eb0d81bc9e53b1a10b6
疒奀 <46226846+mlegls@users.noreply.github.com>
2024-05-31 21:12:32 +0800

    remove backup comments

--- src/run-job.ts
+++ src/run-job.ts
@@ -84,17 +84,6 @@ const main = async () => {
 
   // failure
   if (!hiveRunOut.includes("Results accepted.")) {
-    /*
-    await db
-      .prepare(
-        `INSERT INTO ${tableName} 
-        (uuid, ts_start, ts_end, command, status_code, addr_requester) 
-        VALUES (?, ?, ?, ?, ?, ?);`
-      )
-      .bind(runUuid, tsStart, tsEnd, commandBody.join(" "), 1, pubKey)
-      .run()
-      .then(({ meta: insert }) => insert.txn?.wait());
-    */
     await tablelandInsert(tableName as string, {
       uuid: runUuid,
       ts_start: tsStart,
@@ -103,6 +92,7 @@ const main = async () => {
       status_code: 1,
       addr_requester: pubKey,
     });
+
     throw new Error("Job failed");
   }
 
@@ -124,30 +114,6 @@ const main = async () => {
   const inspectOutLines = hiveInspectOut.split("\n");
   const lastLine = inspectOutLines[inspectOutLines.length - 2];
   const runInfo = JSON.parse(lastLine);
-
-  /*
-  await db
-    .prepare(
-      `INSERT INTO ${tableName} 
-      (uuid, ts_start, ts_end, command, result_ipfs_url, status_code, 
-        addr_requester, addr_resource_provider, addr_mediator, addr_solver) 
-      VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?);`
-    )
-    .bind(
-      runUuid,
-      tsStart,
-      tsEnd,
-      commandBody.join(" "),
-      ipfsUrl,
-      0,
-      pubKey,
-      runInfo["Members"]["ResourceProvider"],
-      runInfo["Members"]["Mediators"][0], // TODO change table schema to fit SharedStructsDeal
-      runInfo["Members"]["Solver"]
-    )
-    .run()
-    .then(({ meta: insert }) => insert.txn?.wait());
-  */
 
   await tablelandInsert(tableName as string, {
     uuid: runUuid,
